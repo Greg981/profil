a5c5932556b858577c8fb98ed6d8d1df7ba200d5
Greg <gregory.grosse@outlook.com>
2019-11-05 16:05:59 +0100

    12h45

--- profiljs1.js
+++ profiljs1.js
@@ -1,1 +1,13 @@
-inputInstance.indeterminate = true;
+// inputInstance.indeterminate = true;
+
+function cache(id){
+    document.getElementById(id).style.display = "none"; }
+
+function montre(id){
+    document.getElementById(id).style.display = "block"; }
+
+const checkBoxes = document.querySelectorAll('input[type="checkbox"]');
+
+if(checkBoxes[0].getAttribut('checked') == true) {
+    montre()
+}
